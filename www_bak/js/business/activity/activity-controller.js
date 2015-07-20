3f925af599fa6ccf75e22339ff86322ac8a045dc
Icepo <104494108@qq.com>
2015-07-20 09:32:09 +0800

    D20150720 DEL

--- www_bak/js/business/activity/activity-controller.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by liuzhangjun on 2015-7-3.
- */
-app.controller('activityController',function($scope,$state){
-    $scope.activity_items=['1','2'];
-    $scope.activityDetail=function(id){
-        $state.go('activity.detail');
-    };
-});
--- www_bak/js/system/service/utilService.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by liuzhangjun on 2015-6-21.
- * 工具服务类
- */
